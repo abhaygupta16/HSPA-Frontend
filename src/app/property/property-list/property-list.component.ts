70c328066c639d43f2708f0b579153272779ede4
abhay <guptaabhay.gpt16@gmail.com>
2021-04-26 15:25:13 +0530

    created a Service for displaying property data using Api

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty';
 
 @Component({
   selector: 'app-property-list',
@@ -7,36 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  properties: any =[
-    {
-      "Id":1,
-      "Name":"Birla House",
-      "Type":"House",
-      "Price":12000
-    },
-    {
-      "Id":2,
-      "Name":"Tata House",
-      "Type":"House",
-      "Price":14000
-    },
-    {
-      "Id":3,
-      "Name":"Ambani House",
-      "Type":"House",
-      "Price":24000
-    },
-    {
-      "Id":4,
-      "Name":"Adani House",
-      "Type":"House",
-      "Price":18000
-    }
-  ]
+  properties: Array<IProperty> ;
 
-  constructor() { }
+  constructor( private housingService : HousingService) { }
 
   ngOnInit() {
+    this.housingService.getAllProperties()
+                        .subscribe(data => this.properties=data,error => console.log(error))
   }
 
 }

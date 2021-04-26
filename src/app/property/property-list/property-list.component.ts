e8676e3a1830fb4890bb15429882e989a1cde2d6
abhay <guptaabhay.gpt16@gmail.com>
2021-04-26 14:53:30 +0530

    created property-card template(html)

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: any =[
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Tata House",
+      "Type":"House",
+      "Price":14000
+    },
+    {
+      "Id":3,
+      "Name":"Ambani House",
+      "Type":"House",
+      "Price":24000
+    },
+    {
+      "Id":4,
+      "Name":"Adani House",
+      "Type":"House",
+      "Price":18000
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {

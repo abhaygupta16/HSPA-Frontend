745812a0f6813df2d83d9c1c11233802b7ce9495
Abhay Gupta <guptaabhay.gpt16@gmail.com>
2021-05-06 12:47:15 +0530

    adding custom sort using pipes

--- src/app/Pipes/sort.pipe.ts
+++ src/app/Pipes/sort.pipe.ts
@@ -5,8 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: Array<string>, args: any[]): any {
+
+    const sortField = args[0];        //field that will help in sorting
+    const sortDirection = args[1];    //feild to set direction of sort (asc or desc)
+    let multiplier = 1;             //used to reverse the sort value in case direction is desc
+
+    if(sortDirection === 'desc'){
+      multiplier = -1;
+    }
+
+    //by default the sort method of array returns in asc order
+    value.sort((a: any,b: any) => {
+
+      if(a[sortField] < b[sortField]){
+        return -1 * multiplier;
+      }else if(a[sortField] > b[sortField]){
+        return 1 * multiplier;
+      }else{
+        return 0;
+      }
+
+    });
+
+    return value;
   }
 
 }
